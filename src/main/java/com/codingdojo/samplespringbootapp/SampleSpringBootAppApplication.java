8c073e740acba1b08f959167fe9cacfb0d2f149e
manaralolasi <manaralalasi2030@gmail.com>
2022-08-28 14:11:44 +0300

    first commit

---
+++ src/main/java/com/codingdojo/samplespringbootapp/SampleSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.samplespringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootAppApplication.class, args);
+	}
+
+}
